7d12edc5d4387d72c47d8cc1af95ca9d9d0e2940
Francis Jemuel Bergonia <francisyuweh@Franciss-MacBook-Pro.local>
2017-09-29 03:15:06 +0800

    Initial Commit

---
+++ GoalPost-App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GoalPost-App
+//
+//  Created by Francis Jemuel Bergonia on 29/09/2017.
+//  Copyright © 2017 Francis Jemuel Bergonia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
